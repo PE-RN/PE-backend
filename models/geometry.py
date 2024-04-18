a0d234f9e9100fd24d9738af86c302fdc7cca0c5
Rodolfo Dantas de Oliveira Bezerra <47856330+denpufa@users.noreply.github.com>
2024-04-18 14:20:20 -0300

    feature(Rodolfo): Organizar projeto em camadas e ORM (#6)

--- models/geometry.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class GeometryModel(BaseModel):
-    type: str
-    coordinates: list
--- models/properties.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class PropertiesModel(BaseModel):
-    name: str | None = None
-    description: str | None = None
