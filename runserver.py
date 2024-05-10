9ec506459d875687160d64eab8299ecb5904cd60
RafaelDRA <r.dra@hotmail.com>
2024-05-10 14:01:21 -0300

    try

--- runserver.py
+++ runserver.py
@@ -1,6 +1,28 @@
-from os import getenv
+import os
+import subprocess
 from uvicorn import run
 
+def install_dependencies():
+    # Install system dependencies, uncomment and modify according to your needs
+    # subprocess.run(["sudo", "apt-get", "update"], check=True)
+    # subprocess.run(["sudo", "apt-get", "install", "-y", "libgdal-dev"], check=True)
+    # Environment variables needed for GDAL
+    # os.environ['CPLUS_INCLUDE_PATH'] = '/usr/include/gdal'
+    # os.environ['C_INCLUDE_PATH'] = '/usr/include/gdal'
+    
+    # Install Python packages
+    subprocess.run(["pip", "install", "--upgrade", "pip"], check=True)
+    subprocess.run(['apt','install','gdal-bin', 'python3-gdal'],check=True)
+    subprocess.run(["pip", "install", "GDAL", "rasterio"], check=True)  # Add other packages as needed
+    # Install from requirements.txt if you have one
+    subprocess.run(["pip", "install", "-r", "requirements.txt"], check=True)
+
+def main():
+    # Install dependencies before starting the server
+    install_dependencies()
+    
+    # Run the FastAPI application with Uvicorn
+    run(host='0.0.0.0', port=int(os.getenv('PORT', '5000')), app='main:app', workers=4)
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=int(getenv('PORT','5000')),app='main:app',workers=4)
+    main()
