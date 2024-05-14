33005569570e3f5b91479373cc785026e5d2c5d5
Rodolfo Dantas de Oliveira Bezerra <47856330+denpufa@users.noreply.github.com>
2024-05-14 16:07:41 -0300

    Pe 49 criacao de estrutura de login back end (#14)
    * config development
    
    * fix(Rodolfo) : corrgir lint erros
    
    * feature(Rafael): Modificar script e add CORS
    
    * feature(Rodolfo): Organizar projeto em camadas e ORM (#6)
    
    * bugix(Rafael): fix na verificação de polígonos (#7)
    
    * feat(Sophia): Atualizando README com definições de merge (#8)
    
    Co-authored-by: Sophia Victoria Santos <sophiasantos@isi-er.com.br>
    
    * Pe 62 adicionar script para carregar valores de raster (#10)
    
    * bugix(Rafael): fix na verificação de polígonos
    
    * Feature(Rafael): Adicionar script
    
    * feature(Rafael): nova função para rasters (#11)
    
    * feature(Rafael): nova função para rasters
    
    * feature(Rafael): nova função para rasters
    
    * init login struct
    
    * feature(Rodolfo): Criação de login e async db acesso
    
    * fix(Rodolfo): deletar não necessario .bandit file
    
    * fix(Rodolfo):  Ajustar importações
    
    * feature(Rodolfo): construição de testes unitários
    
    * feature(Rodolfo): Criação de estrutura de login
    
    * fix(Rodolfo): Ajustar requiriments.txt
    
    * fix(Rodolfo): config circle CI file
    
    * fix(Rodolfo): Ajustar comando root
    
    * fix(Rodolfo): Adicionar gdal
    
    * fix(Rodolfo): instalar gdal
    
    * fix(Rodolfo): Ajuste de env
    
    * fix(Rodolfo): Ajustar CI
    
    * fix(Rodolfo): Ajustar req txt
    
    * fix(Rodolfo): Ajustar gdal install
    
    * fix(Rodolfo): Ativar env
    
    * fix(Rodolfo): teste
    
    * fix(Rodolfo): Ajustar bin
    
    * fix(Rodolfo): env file
    
    * fix(Rodolfo): variables env
    
    * fix(Rodolfo): envs variables
    
    * fix(Rodolfo): Testar arquivo CI
    
    * fix(Rdodolfo): Adjust config file
    
    * fix(Rodolfo): Ajustar CI
    
    * fix(Rodolfo): variaveis
    
    * fix(Rodolfo): Ajustar config bandit fala
    
    * fix(Rodolfo): AJustart CI file
    
    * fix(Rodolfo): Ajustar os files
    
    * fix(Rodolfo): Ajustar CI
    
    ---------
    
    Co-authored-by: RafaelDRA <r.dra@hotmail.com>
    Co-authored-by: sophivs <99304777+sophivs@users.noreply.github.com>
    Co-authored-by: Sophia Victoria Santos <sophiasantos@isi-er.com.br>

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", "https://plataforma-energias-rn-production.up.railway.app/"],  # Allows only requests from 
+    allow_origins=["http://localhost:8000"],  # Allows only requests from localhost:8000
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
