8b8fe0be05e5b5d78137128fa1a4367e311f4a6d
RafaelDRA <r.dra@hotmail.com>
2024-06-04 10:16:43 -0300

    feat(Patrick): Ajustar interface de rede railway

--- runserver.py
+++ runserver.py
@@ -3,7 +3,7 @@ from uvicorn import run
 
 
 def main():
-    run(host='127.0.0.1', port=int(os.getenv('PORT', '5000')), app='main:app', workers=4)
+    run(host=os.getenv('HOST','localhost'), port=int(os.getenv('PORT', '5000')), app='main:app', workers=4)
 
 
 if __name__ == '__main__':
