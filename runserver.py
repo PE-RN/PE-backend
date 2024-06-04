35c4b273baf969622f0fa848e277bdb28b5e2803
RafaelDRA <r.dra@hotmail.com>
2024-06-04 09:59:23 -0300

    fix(Rafael): Interface de rede railway

--- runserver.py
+++ runserver.py
@@ -3,7 +3,7 @@ from uvicorn import run
 
 
 def main():
-    run(host='0.0.0.0', port=int(os.getenv('PORT', '5000')), app='main:app', workers=4)
+    run(host='127.0.0.1', port=int(os.getenv('PORT', '5000')), app='main:app', workers=4)
 
 
 if __name__ == '__main__':
