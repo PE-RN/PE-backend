d8cddbe198ed72874cf32f4e95c3f140e1336f78
Rodolfo Dantas <rodolfobez15@gmail.com>
2024-06-11 09:25:06 -0300

    feature(Rodolfo): corrigir testes

--- test/geo_repository_test.py
+++ test/geo_repository_test.py
@@ -1,5 +1,5 @@
 import os
-from unittest.mock import AsyncMock, MagicMock
+from unittest.mock import AsyncMock, MagicMock, Mock
 
 import pytest
 from osgeo import gdal
@@ -16,44 +16,39 @@ test_get_raster_parameters = [
 @pytest.mark.asyncio
 @pytest.mark.parametrize("filename, query_result, x, y, z, expected", test_get_raster_parameters)
 async def test_get_raster(filename, query_result, x, y, z, expected):
-    # Mocking the necessary dependencies
-    mock_db = AsyncMock()
-    mock_result = MagicMock()
-    mock_result.fetchone.return_value = (query_result)
-    mock_db.execute.return_value = mock_result
-    mock_db.execute.return_value.fetchone.return_value = query_result
 
-    your_object = GeoRepository(db=mock_db)
+    # Arrange
+    mock_db = MagicMock()
+    geo_repository = GeoRepository(db=MagicMock())
+    mock_db.fetchone.return_value = (query_result)
+    geo_repository.db.execute = AsyncMock(return_value=mock_db)
 
-    # Testing the method
-    raster_data = await your_object.get_raster(filename, x, y, z)
+    # Act
+    raster_data = await geo_repository.get_raster(filename, x, y, z)
+
+    # Assert
     assert raster_data == expected
 
 test_get_raster_dataset_parameters = [
     ('wrong_filename', None, 'dataset', None),
     ('correct_filename', (b'test',), 'dataset', 'dataset'),
-    ('correct_filename_no_list', b'test', 'dataset', None)
 ]
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("filename, query_result, dataset, expected", test_get_raster_dataset_parameters)
 async def test_get_raster_dataset(filename, query_result, dataset, expected):
-    # Mocking the necessary dependencies
-    mock_db = AsyncMock()
-    mock_result = MagicMock()
-    mock_result.fetchone.return_value = (query_result)
-    mock_db.execute.return_value = mock_result
-    mock_db.execute.return_value.fetchone.return_value = query_result
 
-    mock_gdal_open = MagicMock()
-    mock_gdal_open.return_value = dataset
-    gdal.Open = mock_gdal_open
-    mock_os_remove = MagicMock()
-    os.remove = mock_os_remove
+    mock_db = MagicMock()
+    # Arrange
+    mock_db.fetchone.return_value = (query_result)
+    geo_repository = GeoRepository(db=MagicMock())
+    geo_repository.db.execute = AsyncMock(return_value=mock_db)
+    gdal.Open = Mock(return_value=dataset)
+    os.remove = Mock(return_value=None)
 
-    your_object = GeoRepository(db=mock_db)
+    # Act
+    raster_data = await geo_repository.get_raster_dataset(filename)
 
-    # Testing the method
-    raster_data = await your_object.get_raster_dataset(filename)
+    # Assert
     assert raster_data == expected
