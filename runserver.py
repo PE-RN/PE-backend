8e3e88b5e32948ac7b819b529a3dae9aa584938a
Rodolfo Dantas de Oliveira Bezerra <47856330+denpufa@users.noreply.github.com>
2024-06-24 10:56:53 -0300

    feature(Rodolfo): Ajustar configuração de workers. (#34)

--- runserver.py
+++ runserver.py
@@ -3,7 +3,7 @@ from uvicorn import run
 
 
 def main():
-    run(host=os.getenv('HOST', 'localhost'), port=int(os.getenv('PORT', '5000')), app='main:app', workers=4)
+    run(host=os.getenv('HOST', 'localhost'), port=int(os.getenv('PORT', '5000')), app='main:app', workers=1)
 
 
 if __name__ == '__main__':
