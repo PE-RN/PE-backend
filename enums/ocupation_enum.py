849d835fd3a18e9c667651e03fbeb7c87512ac50
Rafael Almeida <r.dra@hotmail.com>
2024-08-20 10:45:08 -0300

    refactor(Rafael): classe perfis usuario

--- enums/ocupation_enum.py
+++ enums/ocupation_enum.py
@@ -3,7 +3,11 @@ from enum import Enum
 
 class OcupationEnum(str, Enum):
 
-    INVESTOR = "investidor"
-    RESEARCHER = "pesquisador"
-    DEVELOPER = "desenvolvedor"
-    OTHER = "outro"
+    ACADEMICO = "Acadêmico"
+    EMPREENDEDOR = "Empreendedor"
+    GOVERNAMENTAL = "Governamental"
+    DESENVOLVEDOR = "Desenvolvedor"
+    AMBIENTAL = "Ambiental"
+    SOCIEDADE = "Sociedade Civil"
+    ONG = "ONGs"
+    OUTRO = "Outro"
